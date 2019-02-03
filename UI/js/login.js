09c94cac8fff0575b7f374cf029b1f315dca9bf6
kburudi <bkevery@gmail.com>
2019-02-03 13:38:36 +0300

    [finishes #163681061]fetch login

---
+++ UI/js/login.js
@@ -0,0 +1,106 @@
+let requiredFields = document.querySelectorAll(".reg-input-fields")
+let submitButton = document.querySelector(".reg-input-submit")
+
+console.log("   ".trim().length)
+
+let checkRequired = () => {
+  requiredFields.forEach(element => {
+    element.addEventListener("blur", () => {
+      let myValue = element.value;
+      let inputParent = element.parentNode;
+      let inputSiblings = inputParent.childNodes
+      error = inputSiblings[7]
+      if (myValue.length == 0 || myValue.trim().lenght == 0) {
+        element.style.border = "1px solid red"
+        error.style.display = "inline-block"
+        error.textContent = "Field is required"
+      } else {
+        error.style.display = "none"
+        element.style.border = "1px solid #3877ea"
+        if (myValue.trim() === '') {
+          element.style.border = "1px solid red"
+          error.style.display = "inline-block"
+          error.textContent = "Cannot be whitespace only"
+        }
+      }
+    })
+  });
+
+  submitButton.addEventListener("click", e => {
+    e.preventDefault()
+    requiredFields.forEach(element => {
+      let myValue = element.value;
+      let inputParent = element.parentNode;
+      let inputSiblings = inputParent.childNodes
+      error = inputSiblings[7]
+      if (myValue.length == 0 || myValue.trim().lenght == 0) {
+        element.style.border = "1px solid red"
+        error.style.display = "inline-block"
+        error.textContent = "Field is required"
+      }
+      if (myValue.trim() === '') {
+        element.style.border = "1px solid red"
+        error.style.display = "inline-block"
+        error.textContent = "Cannot be whitespace only"
+      }
+    })
+
+  })
+}
+checkRequired()
+
+userLogin = () => {
+  submitButton.addEventListener("click", e => {
+    e.preventDefault()
+    fetch("https://questioner--api.herokuapp.com/api/v2/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+
+          "username": document.querySelector(".username").value,
+          "password": document.querySelector(".password").value
+        })
+
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.status == 200) {
+          let loggedInSuccess = document.createElement("p");
+          loggedInSuccess.classList.add("loginsuccess")
+          let loginTextPar = document.createElement("p")
+          loggedInSuccess.appendChild(loginTextPar)
+          loginTextPar.textContent = "logging in as " + document.querySelector(".username").value + " ...";
+          let loadImage = document.createElement("img")
+          loadImage.setAttribute("src", "images/loader/source.gif")
+          loadImage.style.width = "100px"
+          loadImage.style.height = "100px"
+          loggedInSuccess.appendChild(loadImage)
+          body = document.querySelector("body")
+          body.appendChild(loggedInSuccess)
+          body.style.opacity = "0.7"
+
+          window.setTimeout(function () {
+            location.href = "https://kburudi.github.io/Questioner-UI/UI/meetups.html"
+          }, 1500);
+        }
+
+        if (data.status === 401) {
+          if (data.error === "unregistered username") {
+            let unameError = document.querySelector(".uname-error")
+            unameError.textContent = data.error
+            unameError.style.display = "inline-block"
+          }
+          if (data.error === "incorrect password") {
+            let passError = document.querySelector(".pass-error")
+            passError.textContent = data.error
+            passError.style.display = "inline-block"
+          }
+        }
+      })
+  })
+
+}
+userLogin()
