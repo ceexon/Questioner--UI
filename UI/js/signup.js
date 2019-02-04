a10aa9c50052b13d5efd88ea4be6a8a86e97af18
kburudi <bkevery@gmail.com>
2019-02-05 00:08:51 +0300

    [feature #163683196]update signup success redirect

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -79,30 +79,27 @@ let userSignUp = () => {
       .then(res => res.json())
       .then(data => {
         // success signup
-        if (data.status === 201) {
+        let thePassword = document.querySelector("#password")
+        let confPassword = document.querySelector("#cpassword")
+        if (thePassword.value != confPassword.value) {
           successMessage.textContent = "User created successfully"
           body = document.querySelector("body")
           body.appendChild(successMessage)
-
-          let thePassword = document.querySelector("#password")
-          let confPassword = document.querySelector("#cpassword")
-          if (thePassword.value != confPassword.value) {
-            submitButton.addEventListener("click", e => {
-              e.preventDefault()
-              let cpassError = document.querySelector(".cpass-error");
-              cpassError.textContent = "passwords do not match"
-              cpassError.style.display = "inline-block"
-            })
-          } else {
+          if (data.status === 201) {
             window.setTimeout(function () {
               location.href = "https://kburudi.github.io/Questioner-UI/UI/signin.html"
             }, 1000);
-          }
 
+          }
           requiredFields.forEach(element => {
             let myValue = element.value;
             myValue.textContent = ""
           })
+        } else {
+          e.preventDefault()
+          let cpassError = document.querySelector(".cpass-error");
+          cpassError.textContent = "passwords do not match"
+          cpassError.style.display = "inline-block"
         }
 
         //handle 409 conflicts
@@ -163,3 +160,31 @@ let userSignUp = () => {
 }
 
 userSignUp()
+
+
+// redirect
+// if (data.status === 201) {
+//   successMessage.textContent = "User created successfully"
+//   body = document.querySelector("body")
+//   body.appendChild(successMessage)
+
+//   let thePassword = document.querySelector("#password")
+//   let confPassword = document.querySelector("#cpassword")
+//   if (thePassword.value != confPassword.value) {
+//     submitButton.addEventListener("click", e => {
+//       e.preventDefault()
+//       let cpassError = document.querySelector(".cpass-error");
+//       cpassError.textContent = "passwords do not match"
+//       cpassError.style.display = "inline-block"
+//     })
+//   } else {
+//     window.setTimeout(function () {
+//       location.href = "https://kburudi.github.io/Questioner-UI/UI/signin.html"
+//     }, 1000);
+//   }
+
+//   requiredFields.forEach(element => {
+//     let myValue = element.value;
+//     myValue.textContent = ""
+//   })
+// }
